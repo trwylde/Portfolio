c10e7c844bdecc36347020d2891247de54ce156b
trwylde <trwylde@gmail.com>
2018-12-11 15:29:42 -0500

    Updated projects showcased on portfolio page. Removed project image carousel and replace with cards.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
   $('.parallax').parallax();
   $('.carousel').carousel();
 
+  var typed = new Typed("#typed", {
+    strings: [
+      "<strong>Full Stack Developer</strong>",
+      "<strong>Web Developer</strong>",
+      "<strong>Web Designer</strong>",
+      "<strong>Frontend Developer</strong>",
+      "<strong>Frontend Designer</strong>"
+    ],
+    typeSpeed: 80,
+    loop: true,
+    backDelay: 1100,
+    backSpeed: 30,
+    // fadeOut: true
+  });
 });
